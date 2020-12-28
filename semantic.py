8c36bbc211320e0c93e9c937eae32564a4d30539
BmxDmx <valeraplamadea@gmail.com>
2020-12-28 19:49:50 +0000

    Valeriu commit

--- semantic.py
+++ semantic.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #### Each if these functions return a mask for essential colours
-# They take the imafge in hsv
+# They take the image in hsv
 def extractWhite(hsv):
-    whiteLower = np.array([20, 20, 150])
-    whiteUpper = np.array([180, 70, 255])
+    whiteLower = np.array([0, 0, 100])
+    whiteUpper = np.array([180, 90, 255])
     #whiteUpper = np.array([196, 216, 255])
     mask = cv2.inRange(hsv,whiteLower,whiteUpper)
     return mask
@@ -39,12 +39,15 @@ def extractBlack(hsv):
 
 
 def createMask(name,x1,x2,y1,y2):
-    img = cv2.imread('./Training/img/'+str(name)+'.png', cv2.IMREAD_COLOR) 
+    # img = cv2.imread('./Training/img/'+str(name)+'.png', cv2.IMREAD_COLOR) 
     
-    #rectROI = cv2.rectangle(316,397,445,557)
-    imgROI = img[int(y1):int(y2),int(x1):int(x2)]                    # [y1:y2,x1:x2]
-    cv2.imshow('imgROI',imgROI)
-    resised_img = cv2.resize(imgROI,(853,480))
+    # #rectROI = cv2.rectangle(316,397,445,557)
+    # imgROI = img[int(y1):int(y2),int(x1):int(x2)]                    # [y1:y2,x1:x2]
+    # cv2.imshow('imgROI',imgROI)
+
+    
+    img = cv2.imread('../Training/img/68.png',cv2.IMREAD_COLOR)
+    resised_img = cv2.resize(img,(853,480))
 
     #Img color conversion
     gray = cv2.cvtColor(resised_img,cv2.COLOR_BGR2GRAY)
@@ -58,11 +61,11 @@ def createMask(name,x1,x2,y1,y2):
     mask_black = extractBlack(hsv)
 
     #Result
-    # res_blue = cv2.bitwise_and(resised_img,resised_img,mask=mask_blue)
-    # res_white = cv2.bitwise_and(resised_img,resised_img,mask=mask_white)
-    # res_orange = cv2.bitwise_and(resised_img,resised_img,mask=mask_orage)
-    # res_yellow = cv2.bitwise_and(resised_img,resised_img,mask=mask_yellow)
-    # res_black = cv2.bitwise_and(resised_img,resised_img,mask=mask_black)
+    res_blue = cv2.bitwise_and(resised_img,resised_img,mask=mask_blue)
+    res_white = cv2.bitwise_and(resised_img,resised_img,mask=mask_white)
+    res_orange = cv2.bitwise_and(resised_img,resised_img,mask=mask_orange)
+    res_yellow = cv2.bitwise_and(resised_img,resised_img,mask=mask_yellow)
+    res_black = cv2.bitwise_and(resised_img,resised_img,mask=mask_black)
 
 
     #Added images
@@ -72,38 +75,8 @@ def createMask(name,x1,x2,y1,y2):
     addedYellowBlackMask = cv2.addWeighted(mask_yellow,0.5,mask_black,0.5,1)
 
 
-    #Edges
-
-    # edges = cv2.Canny(added_together,200,200)
-
-    # cv2.imshow('edges',edges)
 
 
-    #Blurring and Smotthing and Morphological transformation but it doesn't  really work
-
-    # kernel = np.ones((2,2),np.uint8)
-    # erosion = cv2.erode(mask_yellow,kernel,iterations = 1)
-    # erosionBlack = cv2.erode(mask_black,kernel,iterations=1)
-    # dialetion = cv2.dilate(mask_yellow,kernel,iterations = 1)
-    #blurr = cv2.bilateralFilter(res_yellow,15,75,75)
-
-    # added_blue1 = cv2.addWeighted(res_blue,0.8,erosion,0.2,0)
-    # added_together1 = cv2.addWeighted(added_blue1,0.5,added_yellow,0.5,10)
-
-
-    #cv2.imshow('blurr',blurr)
-    #cv2.imshow('erosion',erosion)
-    #cv2.imshow('erosionBlack',erosionBlack)
-    #cv2.imshow('dialetion',dialetion)
-    #cv2.imshow('added_together1',added_together1)
-    #cv2.imshow('addedYellowBlackMask',addedYellowBlackMask)
-
-
-
-    #Thresholds
-    # ret, thresh = cv2.threshold(resised_img,150,255,cv2.THRESH_BINARY_INV)
-    # ret2,thresh_gray = cv2.threshold(gray,150,255,cv2.THRESH_BINARY_INV)
-    # thresh_inv = cv2.bitwise_not(thresh)
 
 
     #Display all the images
@@ -130,9 +103,11 @@ def createMask(name,x1,x2,y1,y2):
 
     # cv2.imshow('added_yellow',added_yellow)
     # cv2.imshow('added_blue',added_blue)
-    #cv2.imshow('added_together',added_together)
+    # cv2.imshow('added_together',added_together)
 
 
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+createMask(1,1,1,1,1)
