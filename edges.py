02c44c1a939f1482e5976b9c4e194dd4ecebd2a1
BmxDmx <valeraplamadea@gmail.com>
2020-12-29 18:59:40 +0000

    Delete edges.py

--- edges.py
+++
@@ -1,8 +0,0 @@
-#Edges
-laplacian = cv2.Laplacian(added_together, cv2.CV_64F)
-soblex = cv2.Sobel(added_together,cv2.CV_64F,1,0,ksize=5)
-sobley = cv2.Sobel(added_together,cv2.CV_64F,0,1,ksize=5)
-
-cv2.imshow('laplacian',laplacian)
-cv2.imshow('soblex',soblex)
-cv2.imshow('sobley',sobley)
